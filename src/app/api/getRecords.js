754bce62e117c9158765c0ac0811e1c83f065f1a
Rodri <rodrogo92@gmail.com>
2025-02-05 13:07:49 +0100

    probando vercel api

---
+++ src/app/api/getRecords.js
@@ -0,0 +1,50 @@
+const mysql = require('mysql2');  // Usamos el cliente mysql2
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST || '192.168.210.102',  // Dirección de tu base de datos
+  user: process.env.DB_USER || 'caseta_lama',      // Usuario de la base de datos
+  password: process.env.DB_PASS || 'caseta_lama',  // Contraseña de la base de datos
+  database: process.env.DB_NAME || 'caseta_lama',  // Nombre de la base de datos
+  port: process.env.DB_PORT || 3306,                // Puerto de conexión, por defecto es 3306
+  connectionLimit: 5,                               // Límite de conexiones simultáneas
+  acquireTimeout: 5000                              // Tiempo máximo de espera para obtener una conexión
+});
+
+module.exports = async (req, res) => {
+  // Realizamos la consulta a la tabla de registros
+  pool.query('SELECT * FROM registros', (err, results) => {
+    if (err) {
+      return res.status(500).json({ message: "Error de la base de datos", error: err });
+    }
+
+    return res.status(200).json(results);  // Devolvemos los resultados obtenidos de la base de datos
+  });
+};
+
+// Consulta de socios
+app.get('/api/socios', (req, res) => {
+  const query = `
+    SELECT
+      socios.idsocio,
+      socios.nombre,
+      socios.apellido,
+      socios.telefono,
+      socios.direccion,
+      socios.email,
+      socios.invitaciones,
+      socios.poblacion,
+      socios.dni
+    FROM
+      socios
+  `;
+
+  // Usamos pool.query() para la consulta de socios
+  pool.query(query, (err, rows) => {
+    if (err) {
+      console.error('Error en la consulta:', err);
+      return res.status(500).json({ error: 'Error al obtener los socios' });
+    }
+
+    return res.json(rows);  // Enviamos los datos de los socios como respuesta JSON
+  });
+});
